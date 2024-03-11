0985b624355b319d3e9d647a0d9b3f451d31a0c1
Qorri Dwi Istajib <qorri24j@gmail.com>
2024-03-11 10:27:11 +0700

    first commit

---
+++ wadools-country/src/main/java/com/qodev/tech/common/Constants.java
@@ -0,0 +1,12 @@
+package com.qodev.tech.common;
+
+public class Constants {
+
+    public static final int MAX_LIMIT = 100;
+    public static final int ERROR_CODE_DATA_EXIST = 202;
+    public static final int ERROR_CODE_REQUIRED = 500;
+    public static final String SUCCESS = "SUCCESS";
+    public static final String ERROR = "ERROR";
+
+
+}
